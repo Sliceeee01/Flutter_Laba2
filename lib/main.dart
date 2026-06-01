300e3681bce1398ead2d255bb129088048aa324b
Sliceeee01 <ivan_hih@icloud.com>
2026-06-01 18:25:23 +0300

    Explore project structure and VS Code tools

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('ВЫ нажали кнопку столько раз:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
